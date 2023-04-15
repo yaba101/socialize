e68e5a9f3bf882185b0ef65b505aad4918b99cd1
Yeabsira Mekuria <Yabumek46@gmail.com>
2023-04-15 23:25:24 +0300

    modify dashboard UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,23 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import SignupPage from './pages/Signup'
 import LoginPage from './pages/Login'
 import Dashboard from './pages/Dashboard'
-
-import { Cookies } from 'react-cookie'
 import { AuthContextProvider } from './components/AuthProvider'
-
-const cookies = new Cookies()
 
 function App() {
 	return (
-		<div className='flex items-center justify-center h-screen min-h-full px-4 py-12 sm:px-6 lg:px-8'>
-			<div className='w-full max-w-md space-y-8'>
-				<BrowserRouter>
-					<AuthContextProvider>
-						<Routes>
-							<Route path='/login' element={<LoginPage />} />
-							<Route path='/*' element={<Navigate to='/login' />} />
-							<Route path='/signup' element={<SignupPage />} />
-							<Route path='/dashboard' element={<Dashboard />} />
-						</Routes>
-					</AuthContextProvider>
-				</BrowserRouter>
-			</div>
+		<div>
+			<BrowserRouter>
+				<AuthContextProvider>
+					<Routes>
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/*' element={<Navigate to='/login' />} />
+						<Route path='/signup' element={<SignupPage />} />
+						<Route path='/dashboard' element={<Dashboard />} />
+					</Routes>
+				</AuthContextProvider>
+			</BrowserRouter>
 		</div>
 	)
 }
 
 export default App
-
-// create isAuthenticated function to check if user is logged in
-const isAuthenticated = () => {
-	const token = cookies.get('token')
-	return token ? true : false
-}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,27 +1,84 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../components/AuthProvider'
 import { Navigate } from 'react-router-dom'
 import Avatar from '../components/Avatar'
+import PostModal from '../components/PostModal'
 
 const Dashboard = () => {
-	const { isLoggedIn, logout, username } = useContext(AuthContext)
-	if (!isLoggedIn) return <Navigate to='/login' />
+	const { isLoggedIn, logout, setIsLoggedIn } = useContext(AuthContext)
+	const [showCreateModal, setShowCreateModal] = useState(false)
+	const [username, setUsername] = useState(localStorage.getItem('username'))
+
+	const handleCreateClick = () => {
+		setShowCreateModal(true)
+	}
+	const handleCloseClick = () => {
+		setShowCreateModal(false)
+	}
+
+	useEffect(() => {
+		const token = localStorage.getItem('username')
+		if (token) {
+			setIsLoggedIn(true)
+		}
+	}, [setIsLoggedIn])
+	if (!isLoggedIn && !localStorage.getItem('username')) {
+		return <Navigate to='/' />
+	}
+	console.log(username)
+
 	return (
-		<div className='flex flex-col items-center justify-center'>
-			<Avatar username={username} size='lg' className='mr-4' />
-			<h1 className='mb-10 text-2xl font-bold text-gray-200'>
-				Welcome to the Home Page, {username}!
-			</h1>
-			<button className='w-full px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline'>
-				Link Socials
-			</button>
-			<button
-				className='w-48 px-4 py-2 mt-10 font-bold text-white rounded bg-cyan-600 hover:bg-cyan-800 focus:outline-none focus:shadow-outline'
-				onClick={logout}>
-				Log Out
-			</button>
-		</div>
+		<>
+			<div className='max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8 '>
+				<Avatar username={username ?? ''} size='lg' className='mr-4' />
+				<div className='sm:flex sm:items-center sm:justify-between'>
+					<div className='text-center sm:text-left'>
+						<h1 className='text-2xl font-bold text-gray-200 '>
+							Welcome Back, {username}!
+						</h1>
+						<p className='mt-1.5 text-sm text-gray-500 text-center'>
+							Let's write a new post! 🎉
+						</p>
+					</div>
+					<div className='flex flex-col gap-4 mt-4 sm:mt-0 sm:flex-row sm:items-center'>
+						<button
+							className='inline-flex items-center justify-center px-5 py-3 text-gray-500 transition border border-gray-200 rounded-lg hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring'
+							type='button'>
+							<span className='text-sm font-medium'> View Post History </span>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								className='ml-1.5 h-4 w-4'
+								fill='none'
+								viewBox='0 0 24 24'
+								stroke='currentColor'
+								strokeWidth='2'>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+								/>
+							</svg>
+						</button>
+						<button
+							className='block px-5 py-3 text-sm font-medium text-white transition bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring'
+							onClick={handleCreateClick}
+							type='button'>
+							Create Post
+						</button>
+						<button
+							className='block px-3 py-3 text-sm font-medium text-white transition rounded-full bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring'
+							onClick={() => {
+								logout()
+								localStorage.removeItem('username')
+							}}>
+							Log Out
+						</button>
+					</div>
+				</div>
+			</div>
+
+			{showCreateModal && <PostModal onClose={handleCloseClick} />}
+		</>
 	)
 }
-
 export default Dashboard
